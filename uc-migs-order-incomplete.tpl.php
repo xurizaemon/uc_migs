589ec5e471572f8bcab84ef5bd051b63998b29e7
Chris Burgess <chris@giantrobot.co.nz>
2010-06-14 02:55:15 +0000

    Replaces 'Unknown error' with 'Transaction could not be processed' as per comment by Karu on [#568338]

--- uc-migs-order-incomplete.tpl.php
+++ uc-migs-order-incomplete.tpl.php
@@ -4,7 +4,6 @@
  *
  * Completed orders are not themeable in the payment module; you
  * need to theme the Ubercart order completion page instead.
- *
  */
  ?>
 <div class="uc-migs-order-incomplete">
@@ -12,9 +11,9 @@
     An error has occurred during payment.  View the response below and either re-try your order with another method or contact us for help.
   </p>
   <ul>
-    <li>Response description: <strong><?php print check_plain($response_message) ; ?></strong></li>
-    <li>Payment gateway message: <strong><?php print check_plain($message) ; ?></strong></li>
-    <li>Response code: <strong><?php print check_plain($response_code); ?></strong></li>
+    <li>Response description: <strong><?php print $response_message ; ?></strong></li>
+    <li>Payment gateway message: <strong><?php print $message ; ?></strong></li>
+    <li>Response code: <strong><?php print $response_code ; ?></strong></li>
   </ul>
   <p><?php print l('View your shopping cart to attempt payment again.', 'cart') ; ?></p>
 </div>
