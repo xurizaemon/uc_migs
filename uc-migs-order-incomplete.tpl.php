cc57bd8a1a2fcd7b0f95ddd747d44036523c2904
Chris Burgess <chris@giantrobot.co.nz>
2010-06-14 04:54:15 +0000

    Don't say order failed if it's already been processed.

--- uc-migs-order-incomplete.tpl.php
+++ uc-migs-order-incomplete.tpl.php
@@ -12,6 +12,7 @@
  */
  ?>
 <div class="uc-migs-order-incomplete">
+<?php if ( $response_message != 'Order already completed.') : ?>
   <p>
     An error has occurred during payment.  View the response below and either re-try your order with another method or contact us for help.
   </p>
@@ -19,4 +20,16 @@
     <li>Error: <strong><?php print $response_message ; ?></strong></li>
   </ul>
   <p><?php print l('View your shopping cart to attempt payment again.', 'cart') ; ?></p>
+<?php else : ?>
+  <p>
+    It looks like your order has been completed already, but somehow 
+    you've reloaded the order confirmation page. This may not be a problem!
+  </p>
+  <p>
+    You might just need to check the mailbox you used to place the
+    order for further instructions, or contact the site administrator
+    directly.
+  </p>
+  <p><?php print theme('item_list', array( l('Log in or visit your account page.', 'user'), l('View your shopping cart.', 'cart'))); ?></p>
+<?php endif ; ?>
 </div>
